b30fd27b7843c6abe2e4864ca6008b7981e435f6
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-18 14:00:34 -0700

    save progress
    Co-authored by: Emma Hellman <ehh801@icloud.com>
    Co-authored by: Lizzie Van Hook <evanhook360@gmail.com>
    Co-authored by: Cody Fritz <clanalia55@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,73 @@
 
 //constructor function
-function Player (totalScore, roundTotal, diceRoll, currentPlayer) {
-  this.totalScore = totalScore,
-  this.roundTotal = roundTotal,
-  this.diceRoll = diceRoll,
-  this.currentPlayer = currentPlayer
+
+function Game(playerTurn, roundScore, totalScore, winner, player1, player2) {
+  this.playerTurn = playerTurn;
+  this.roundScore = roundScore;
+  this.totalScore = totalScore;
+  this.winner = winner;
+  this.player1 = player1;
+  this.player2 = player2;
 }
 
 //prototypes
-Player.prototype.rollTotal = function () {
-  this.diceRoll = Math.floor((Math.random() * 6) +1); 
-  console.log(this.diceRoll)
+Game.prototype.switchTurn = function() {
+  if (this.playerTurn === 1) {
+    this.playerTurn = 2;
+  } else (this.playerTurn === 2); {
+    this.playerTurn = 1;
+  }
 }
 
-Player.prototype.roundScore = function () {
+Game.prototype.addScores = function() {
+   // roundTotal 
+}
+
+Game.prototype.declareWinner = function() {
+  if (this.player1.totalScore >= 100) {
+    this.winner;
+  } else (this.player2.totalScore >= 100) ; {
+    this.winner;
+  };
+};
+
+Game.prototype.rollTotal = function () {
+  let currentRoll = roll();
+  if (currentRoll === 1) {
+    
+  }
+  this.diceRoll = Math.floor((Math.random() * 6) + 1);
+  // console.log(this.diceRoll)
+}
+
+let roll = function() {
+  let dieRoll = Math.floor((Math.random() * 6) + 1);
+  console.log(dieRoll)
+  return dieRoll; 
+}
+
+Game.prototype.roundScore = function () {
   if (this.diceRoll === 1) {
     this.roundTotal = 0;
+    console.log(this.diceRoll)// ERROR - not runs unless 1 is hit.
     //switchturn?
   } else {
-  this.roundTotal += this.diceRoll
-  }
-} 
 
-Player.prototype.totalSum = function () {
+    console.log(this.roundTotal += this.diceRoll);
+    //this.roundTotal += this.diceRoll
+    //console.log(this.diceRoll)
+    this.roundTotal += this.diceRoll;
+  }
+}
+
+Game.prototype.totalSum = function () {
   this.totalScore += this.roundScore
   if (this.totalScore >= 100) {
-    alert("You won the game!")
+    alert("You won the game!");
     resetGame();
   }
   return this.totalScore
 }
-
-Player.prototype.changeTurn = function() {
-  if (this.currentPlayer === true) {
-    this.currentPlayer = false;
-  } else {
-    this.currentPlayer = true;
-    }
-  }
 
 function resetGame() {
   totalScore = 0;
@@ -45,22 +75,26 @@ function resetGame() {
   diceRoll = 0;
 }
 
-$(document).ready(function() {
 
-var player1 = new Player(0, 0, 0, true);
-var player2 = new Player(0, 0, 0, false);
+$(document).ready(function () {
   
-  $("#roll").click(function(event) {
+  let player1 = new Game(0);
+  let player2 = new Game(0);
+
+  $("#roll").click(function (event) {
     event.preventDefault();
-  
+
+    player1.rollTotal();
     if (player1.currentPlayer === true) {
-      player1.rollTotal();
-// Scope Issue console.log(this.diceRoll)
-      player1.roundScore(); //player1.diceRoll
+      // Scope Issue console.log(this.diceRoll)
+
+      //player1.roundScore(); //player1.diceRoll
+
       // if (player1.diceRoll === 1) {
       //   player1.changeTurn();
       //   player2.changeTurn();
       // } 
+      
       $("#rollscore").text(player1.diceRoll);
       $("#player1round").text(player1.roundScore);
 
@@ -69,8 +103,7 @@ var player2 = new Player(0, 0, 0, false);
       if (player2.diceRoll === 1) {
         player2.changeTurn();
         player1.changeTurn();
-      } 
-      player2.roundScore();
+      } else player2.roundScore();
       $("#rollscore").text(player2.diceRoll);
       $("#player2round").text(player2.roundScore);
     }
@@ -80,21 +113,12 @@ var player2 = new Player(0, 0, 0, false);
 })
 
 
-// function totalRoll(roll, player){
-//   if (roll=== 1) {
-//     player.totalScore = 0;
-//     alert("You rolled a 1. Now it's your opponent's turn!");
-//   } 
-//    if (roll>1) {
-//     player.totalScore += roll;
-//   }
-// }
-
-// Player.prototype.roundScore = function () {
-//   if (this.diceRoll === 1) {
-//     this.roundTotal = 0;
-//     //switchturn?
-//   } else {
-//   this.roundTotal += this.diceRoll
-//   }
-// } 
+//roll button
+  //rolls 1-6
+  //adds roll to round total
+  //check if roll is 1
+    //if 1 switch turn
+//hold button
+  //adds round total to player score
+  //check if player won
+  //switch turn
