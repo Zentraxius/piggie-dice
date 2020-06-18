f8b0c973c7054244cd3d3ddc8487d510f77fdee0
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-18 09:24:31 -0700

    add successful dice roller to page
    Co-authored by: Emma Hellman <eeh801@gmail.com>
    Co-authored by: Lizzie Van Hook <evanhook360@gmail.com>

--- README.md
+++ README.md
@@ -9,14 +9,13 @@
 ## Setup/Installation Requirements
 
 #### Open by downloading:
-1. Download or clone this repository from GitHub [here](jggjg)
+1. Download or clone this repository from GitHub [here](https://github.com/keidsvik/hog-cube.git)
 2. Once on your machine, double-click on index.html to open in your default browser.
 
 #### Open with Bash/GitBash:
 1. Clone this repository onto your computer
-`hghg`
-2. Navigate to the `jggjg` directory in your preferred text editor:
-`code`.
+`https://github.com/keidsvik/hog-cube.git`
+2. Navigate to the `hog-cube` directory in your preferred text editor.
 3. Open index.html in preferred browser:
 `open index.html`
 
@@ -26,20 +25,50 @@ Player rolls the dice
 Input: roll
 Output: continue to roll dice until hitting 1.
 
+- Player constructor
+
+- Dice constructor
+- roll function
+
 Player hits 1 with dice
 Input: Dice rolls 1.
 Output: Next player's turn.
 
+- Player constructor
+- turn function
+
+- Dice constructor
+- roll function
+
 Player clicks hold
 Input: Hold.
-Output: Next player's turn.
+Output: Switch player's turn.
+
+- Player constructor
+- hold function
+- switch turn function
 
 Player rolls a 2, 3, 4, 5 or 6.
 Input: Player rolls a 2, 3, 4, 5 or 6.
 Output: Dice roll is added to running total.
 
+- Player constructor
+- hold function
+- switch turn function
+
+- Dice constructor
+- roll function
+
 Player clicks hold after rolling 2, 3, 4, 5, or 6.
 Input: Running total is added to total score and it becomes next player's turn.
+
+- Player constructor
+- hold function
+- switch turn function
+- score keeper
+
+- Dice constructor
+- roll function
 
 Player rolls a 1.
 Input: Player rolls a 1 
@@ -48,6 +77,10 @@ Output: Running total becomes 0 and total score stays the same.
 Player gets to 100
 Input: Player gets 100 or more in their total score.
 Output: Game ends. Alert "Player# is winner!" and resets game.
+
+Additional functionality
+Input: Click on "New Game" button.
+Output: Reset scores.
 
 ## Known Bugs
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,78 @@
 
-function Player (player1) {
-  this.player1 = player1;
-  this.player1Total = p1Total;
-  this.player1Round = p1Round;
+//constructor function
+function Player (totalScore, roundTotal, diceRoll, currentPlayer) {
+  this.totalScore = totalScore,
+  this.roundTotal = roundTotal,
+  this.diceRoll = diceRoll,
+  this.currentPlayer = currentPlayer
+}
+//prototypes
+Player.prototype.rollTotal = function () {
+  this.diceRoll = Math.floor((Math.random() * 6) +1);
+  
 }
 
-function Dice () {
-  let roll = Math.floor((Math.random() * 6) + 1);
-  this.diceRoll = roll;
+Player.prototype.roundScore = function () {
+  if (this.diceRoll === 1) {
+    this.roundTotal = 0;
+    //switchturn?
+  } else {
+  this.roundTotal += this.diceRoll
+  }
+} 
+
+Player.prototype.totalSum = function () {
+  this.totalSore += this.roundScore
+  if (this.totalSore >= 100) {
+    alert("You won the game!")
+    resetGame();
+  }
+  return this.totalScore
 }
 
-Dice.prototype.dieRoll = function (totalScore) {
-  if (this.diceRoll > 2) {
-    return 
-  } else if (this.diceRoll < 2) {
-    return 
-}
+Player.prototype.changeTurn = function() {
+  if (this.currentPlayer === true) {
+    this.currentPlayer = false;
+  } else {
+    this.currentPlayer = true;
+    }
+  }
 
+function resetGame() {
+  totalScore = 0;
+  roundTotal = 0;
+  diceRoll = 0;
+}
 
 $(document).ready(function() {
-  $("form#thegame").submit(function(event) {
-    event.preventDefault();
 
+var player1 = new Player(0, 0, 0, true);
+var player2 = new Player(0, 0, 0, false);
   
+  $("#roll").click(function(event) {
+    event.preventDefault();
+  
+    if (player1.currentPlayer === true) {
+      player1.rollTotal();
+      console.log(this.diceRoll)
+      if (player1.diceRoll === 1) {
+        player1.changeTurn();
+        player2.changeTurn();
+      } 
+      player1.roundScore(player1.diceRoll);
+      $("#rollscore").text(player1.diceRoll);
+      $("#player1round").text(player1.roundScore);
+    } else if (player2.currentPlayer === true) {
+      player2.rollTotal();
+      if (player2.diceRoll === 1) {
+        player2.changeTurn();
+        player1.changeTurn();
+      } 
+      player2.roundScore();
+      $("#rollscore").text(player2.diceRoll);
+      $("#player2round").text(player2.roundScore);
+    }
+
   })
-});
+
+})
